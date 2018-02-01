a533cb806eb06cd8ef0a510d51dab448a12fcc8d
PatrickColleton <patrickcolleton@gmail.com>
2018-01-31 22:01:18 -0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  root 'posts#index'
+  root 'welcome#index'
 
   resources :posts do
     resources :comments
   end
+
+
+  get '/welcome', to: 'welcome#index'
+  get '/about', to: 'about#index'
+  get '/download', to: 'download#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
